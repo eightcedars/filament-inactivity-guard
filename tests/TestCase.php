af89bd57c6f84c9b12440d9cb884e0b278413dc6
eyounelson <24621192+eyounelson@users.noreply.github.com>
2025-09-10 11:02:19 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,7 +10,6 @@ use Filament\FilamentServiceProvider;
 use Filament\Forms\FormsServiceProvider;
 use Filament\Infolists\InfolistsServiceProvider;
 use Filament\Notifications\NotificationsServiceProvider;
-
 use Filament\Support\SupportServiceProvider;
 use Filament\Tables\TablesServiceProvider;
 use Filament\Widgets\WidgetsServiceProvider;
